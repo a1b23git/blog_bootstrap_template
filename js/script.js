e59b5cb444ff1cfb246a9f2d5cb6cca593e68235
a1b23git <a1b23@yandex.ru>
2021-12-04 10:15:59 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('.carousel').carousel({
+    interval: 5000,
+})
+// var myCarousel = document.querySelector('#carouselExampleCaptions')
+// var carousel = new bootstrap.Carousel(myCarousel, {
+//     interval: 2000,
+// }
